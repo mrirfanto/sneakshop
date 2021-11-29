98e1a41b28e712216e117edf9cd2d76c8e3b4c0f
mrirfanto <mrirfanto.work@gmail.com>
2021-11-30 05:24:29 +0700

    setup redux

---
+++ frontend/src/store.js
@@ -0,0 +1,16 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const reducer = combineReducers({});
+const initialState = {};
+const middleware = [thunk];
+const composeEnhancer =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || composeWithDevTools;
+const store = createStore(
+  reducer,
+  initialState,
+  composeEnhancer(applyMiddleware(...middleware))
+);
+
+export default store;

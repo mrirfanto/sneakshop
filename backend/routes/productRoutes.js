9d4ff19b64bf7d6f893ed831ee1a1a8426efd86a
mrirfanto <mrirfanto.work@gmail.com>
2021-12-07 23:52:49 +0700

    Setup Delete Product backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,9 +3,14 @@ const router = express.Router();
 import {
   getProductById,
   getProducts,
+  deleteProductById,
 } from "../controllers/productController.js";
+import { admin, protect } from "../middleware/authMiddleware.js";
 
 router.route("/").get(getProducts);
-router.route("/:id").get(getProductById);
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, admin, deleteProductById);
 
 export default router;

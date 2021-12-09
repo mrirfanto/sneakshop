ed2bfe18ec855036f9e532ffe9145d1003ab5d74
mrirfanto <mrirfanto.work@gmail.com>
2021-12-09 14:22:21 +0700

    Setup backend Product reviews and rating

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,6 +6,7 @@ import {
   deleteProductById,
   createProduct,
   updateProduct,
+  createProductReview,
 } from "../controllers/productController.js";
 import { admin, protect } from "../middleware/authMiddleware.js";
 
@@ -15,5 +16,6 @@ router
   .get(getProductById)
   .delete(protect, admin, deleteProductById)
   .put(protect, admin, updateProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
 
 export default router;
